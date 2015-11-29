2b92f4a01beaa712896996c7253b84bba247197d
maaniakaa <maniakaa@gmail.com>
2015-11-29 02:24:31 +0100

    z tablicą dynamiczna na wartosc

--- c08_zd3.cpp
+++ c08_zd3.cpp
@@ -6,6 +6,8 @@ double suma (unsigned int n...)
 {	
 	va_list ap;
 	va_start(ap, unsigned int);
+	double argumenty = va_arg(ap, double);
+	
 	
 	va_end(ap);	
 	return ;
@@ -17,10 +19,12 @@ int main ()
 	cout << "Podaj ilość liczb, których średnią chcesz policzyć: ";
 	cin  >> ilosc;
 	cout << "Podaj te liczby: " << endl;
+	int wartosci = new int [ilosc];
 	for (int obrotnik = 0; obrotnik <= n-1; obrotnik++)
-		cin  >> ;
+		cin  >> wartosci[onrotnik];
 	
 	cout << "Suma podanych liczb to: " << suma (ilosc, ???)	
-		
+	
+	delete []wartosci;
 	return 0;
 }
