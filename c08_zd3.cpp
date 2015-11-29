5cd264d2b6c8d33f3ab7ac46db5ca63248846415
maaniakaa <maniakaa@gmail.com>
2015-11-29 15:00:30 +0100

    z bledami

--- c08_zd3.cpp
+++ c08_zd3.cpp
@@ -2,28 +2,31 @@
 #include <cstdarg>
 using namespace std;
 
-double suma (unsigned int n...)
+double suma (unsigned int n, ...)
 {	
 	va_list ap;
-	va_start(ap, unsigned int);
-	double argumenty = va_arg(ap, double);
-	
-	
+	va_start(ap, n);
+	double razem = 0;
+	for (int obrotnik = 0; obrotnik <= n-1; obrotnik++)
+	{
+		double argumenty = va_arg(ap, double);
+		razem += argumenty;
+	}
 	va_end(ap);	
-	return ;
+	return razem;
 };
 
 int main ()
 {	
-	int ilosc, tablica;
+	int ilosc;
 	cout << "Podaj ilość liczb, których średnią chcesz policzyć: ";
 	cin  >> ilosc;
 	cout << "Podaj te liczby: " << endl;
-	int wartosci = new int [ilosc];
-	for (int obrotnik = 0; obrotnik <= n-1; obrotnik++)
-		cin  >> wartosci[onrotnik];
+	double *wartosci = new double [ilosc];
+	for (int obrotnik = 0; obrotnik <= ilosc-1; obrotnik++)
+		cin  >> wartosci[obrotnik];
 	
-	cout << "Suma podanych liczb to: " << suma (ilosc, ???)	
+	cout << "Suma podanych liczb to: " << suma (ilosc, wartosci[ilosc]) << endl;
 	
 	delete []wartosci;
 	return 0;
